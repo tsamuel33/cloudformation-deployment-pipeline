4106831d7be4205460701527f22cd4dc72b7f6a9
Travis Samuel <samuelt@hss.edu>
2023-05-12 17:01:53 -0400

    Added decorator for calling functions that may encounter Boto3 errors

---
+++ scripts/classes/decorators.py
@@ -0,0 +1,20 @@
+from botocore.exceptions import ClientError, ParamValidationError
+from functools import wraps
+from sys import exit
+
+# Decorator to output Boto3 errors in the format set by the logger so
+# errors are easier to see in the output
+def boto3_clienterror_decorator(logger):
+    def error_decorator(func):
+        @wraps(func)
+        def error_wrapper(*args, **kwargs):
+            try:
+                func(*args, **kwargs)
+            except ClientError as err:
+                logger.error(err)
+                exit()
+            except ParamValidationError as err:
+                logger.error(err)
+                exit()
+        return error_wrapper
+    return error_decorator
