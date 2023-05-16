dda7182e6a3392d17855c09b784a5dccaa0eb127
Samuel <samuelt@hss.edu>
2023-05-16 19:19:59 -0400

    Removed CloudFormation error to use botocore.exceptions instead

--- scripts/classes/errors.py
+++ scripts/classes/errors.py
@@ -1,14 +1,3 @@
-class CloudFormationError(Exception):
-    """Raises an exception when...
-    
-    Attributes:
-        message -- message indicating the specifics of the error
-    """
-
-    def __init__(self, message='Generic error') -> None:
-        self.message = message
-        super().__init__(self.message)
-
 class ConfigurationError(Exception):
     """Raises an exception when...
     
