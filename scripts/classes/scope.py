b99e435726d57dedca30aa5f399279a101acaee4
Samuel <samuelt@hss.edu>
2023-08-02 13:39:55 -0400

    dynamically configure guard command location

--- scripts/classes/scope.py
+++ scripts/classes/scope.py
@@ -65,7 +65,6 @@ class PipelineScope:
         "-r"
     ]
     guard_commands = [
-        "/home/runner/.guard/bin/cfn-guard",
         "validate",
         "-r",
         cfn_guard_dir.as_posix()
@@ -350,6 +349,9 @@ class PipelineScope:
         return rendered_template
 
     def cfn_guard_validate(self, account_number, role_name, check_period, stack_prefix, protection, upload_bucket_name):
+        home_dir = Path.home()
+        guard_exe = home_dir / ".guard" / "bin" /" cfn-guard"
+        self.guard_commands.insert(0, guard_exe.as_posix())
         for template in self.create_list:
             rendered = self.render_template_with_parameters(template, account_number, role_name,
                          check_period, stack_prefix, protection, upload_bucket_name)
